c63edfcc6b434adce3d5275c41aa212841161eca
Tron Nguyen <johniusnguyen22297@gmail.com>
2019-07-25 14:14:07 +0700

    remove gitignore files

--- src/components/TodoButtonGroup/index.js
+++ src/components/TodoButtonGroup/index.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import TodoButton from './TodoButton';
 
 class TodoButtonGroup extends PureComponent {
